706dbfcf6664682843c136129ec298535e4e9637
course <zhizntumannaya@gmail.com>
2025-02-16 22:00:26 +0300

    added DELETE + PATCH handlers

--- main.go
+++ main.go
@@ -4,19 +4,13 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
-//type requestBody struct {
-//	Message string `json:"message"`
-//}
-
-var task Task
-
-type JsonStruct struct {
-}
+//var task Task
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	//var tasks []Task
+	var task Task
 	json.NewDecoder(r.Body).Decode(&task)
 	DB.Create(&task)
 	w.Header().Set("Content-Type", "application/json")
@@ -24,14 +18,56 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(task)
 }
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	var tasks []Task
-	DB.Find(&tasks)
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	task := Task{}
+	vars := mux.Vars(r)
+	key := vars["id"]
+	id, err := strconv.Atoi(key)
+	{
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	}
+	//deleted := task
+	json.NewDecoder(r.Body).Decode(&task)
+	DB.Delete(&Task{}, id)
+	w.WriteHeader(http.StatusNoContent)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tasks)
-	//for _, task := range tasks {
-	//	fmt.Fprintf(w, "Сообщения из БД %s!\n", task.Task)
-	//}
+}
+
+func PatchHandler(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	vars := mux.Vars(r)
+	id := vars["id"]
+	DB.First(&task, id)
+
+	var updateData struct {
+		Task   string `json:"task"`
+		IsDone bool   `json:"is_done"`
+	}
+	json.NewDecoder(r.Body).Decode(&updateData)
+	task.Task = updateData.Task
+	task.IsDone = updateData.IsDone
+
+	DB.Model(&task).Updates(map[string]interface{}{
+		"task":    updateData.Task,
+		"is_done": updateData.IsDone,
+	})
+
+	w.Header().Set("Content-Type", "application/json")
+	//json.NewEncoder(w).Encode(map[string]interface{}{"id": task.ID, "updated": true})
+	json.NewEncoder(w).Encode(task)
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	var task []Task
+	DB.Order("id asc").Find(&task)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+
 	w.WriteHeader(http.StatusOK)
 }
 
@@ -45,5 +81,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/messages", PostHandler).Methods("POST")
 	router.HandleFunc("/api/messages", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/delete/{id}", DeleteHandler).Methods("DELETE")
+	router.HandleFunc("/api/patch/{id}", PatchHandler).Methods("PATCH")
 	http.ListenAndServe(":8084", router)
 }
