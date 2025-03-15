9f50381153b1a57769763561bf4aacf689fcbffb
course <zhizntumannaya@gmail.com>
2025-03-15 12:24:12 +0300

    lesson 9

--- internal/userService/orm.go
+++ internal/userService/orm.go
@@ -2,11 +2,13 @@ package userService
 
 import (
 	"gorm.io/gorm"
+	"myProject/internal/taskService"
 )
 
 type User struct {
 	gorm.Model
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Username string             `json:"username"`
+	Email    string             `json:"email"`
+	Password string             `json:"password"`
+	Tasks    []taskService.Task `json:"tasks" gorm:"foreignKey:UserID"`
 }
